591435935579f7688d6cd520d0e3121752d9c963
Makhmud Tojiboev <79273316+makhmudtojiboev@users.noreply.github.com>
2025-03-01 00:36:08 +0500

    Initial commit

---
+++ bank_account.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    def __init__(self, owner: str, balance: float, account_number: str):
+        self.owner = owner
+        self.balance = balance
+        self.account_number = account_number
+
+    
