6338d7bf2c545a35ffce04877e4eb7dc63e0b77c
Makhmud Tojiboev <79273316+makhmudtojiboev@users.noreply.github.com>
2025-03-01 22:41:20 +0500

    Third commit: User class update: first_name and last_name -> full_name

--- bank_account.py
+++ bank_account.py
@@ -6,14 +6,13 @@ class BankAccount:
         self.account_number = account_number
 
 class User:
-    def __init__(self, first_name: str, last_name: str, phone: str, email: str):
-        self.__first_name = first_name
-        self.__last_name = last_name
+    def __init__(self, full_name: str, phone: str, email: str):
+        self.__full_name = full_name
         self.__phone = phone
         self.__email = User.validate_email(email)
 
     def __str__(self):
-        return f"User - First name: {self.__first_name}, Last name: {self.__last_name}, Phone: {self.__phone}, Email: {self.__email}"
+        return f"User - Full name: {self.__full_name}, Phone: {self.__phone}, Email: {self.__email}"
 
     @staticmethod
     def validate_email(email: str):
@@ -24,20 +23,12 @@ class User:
             return email   
 
     @property
-    def first_name(self):
-        return self.__first_name
+    def full_name(self):
+        return self.__full_name
     
-    @first_name.setter
-    def first_name(self, first_name):
-        self.__first_name = first_name
-    
-    @property
-    def last_name(self):
-        return self.__last_name
-    
-    @last_name.setter
-    def last_name(self, last_name):
-        self.__last_name = last_name
+    @full_name.setter
+    def full_name(self, full_name):
+        self.__full_name = full_name
     
     @property
     def phone(self):
